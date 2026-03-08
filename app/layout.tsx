4267f9a5b6cae1cd5c2c07b4f460197fb3f0986f
Soufianinho <soufianmeskin5@gmail.com>
2026-03-08 22:23:50 +0000

    snap pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,23 @@ fbq('track', 'PageView');`,
         </noscript>
         {/* End Meta Pixel Code */}
         
+        {/* Snap Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', '32960c97-92f4-4917-978b-0b6458e35826', {});
+
+snaptr('track', 'PAGE_VIEW');`,
+          }}
+        />
+        {/* End Snap Pixel Code */}
+        
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(c,l,a,r,i,t,y){
